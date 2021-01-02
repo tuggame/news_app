555283404550595fc2d5c87316c4f7348a7365fb
tuggame <46846562+tuggame@users.noreply.github.com>
2021-01-02 04:51:15 +0300

    test düzeltme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:news_app/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
